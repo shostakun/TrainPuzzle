7fc8bedfc766b4096f215f3baf8ec3a30ce6442f
Matthew Fitzgibbons <shostakun@schubert.local>
2025-01-13 18:05:07 +0900

    Resolved #30. Save and load locale setting, with fallback to system setting.

--- Assets/Scripts/LanguageSwitcher.cs
+++ Assets/Scripts/LanguageSwitcher.cs
@@ -1,14 +1,17 @@
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Localization.Settings;
 using TMPro;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
 
 public class LanguageSwitcher : MonoBehaviour
 {
     public TMP_Dropdown dropdown;
+    const string localeKey = "locale";
 
     void Start()
     {
+        LoadLocale();
         dropdown.ClearOptions();
         var options = new List<TMP_Dropdown.OptionData>();
         foreach (var locale in LocalizationSettings.AvailableLocales.Locales)
@@ -16,12 +19,35 @@ public class LanguageSwitcher : MonoBehaviour
             options.Add(new TMP_Dropdown.OptionData(locale.LocaleName));
         }
         dropdown.AddOptions(options);
-        dropdown.value = LocalizationSettings.AvailableLocales.Locales.IndexOf(LocalizationSettings.SelectedLocale);
-        dropdown.onValueChanged.AddListener(LocaleSelected);
+        dropdown.value = LocalizationSettings.AvailableLocales.Locales.IndexOf(
+            LocalizationSettings.SelectedLocale);
+        dropdown.onValueChanged.AddListener(HandleLocaleSelected);
     }
 
-    public static void LocaleSelected(int index)
+    public static void HandleLocaleSelected(int index)
     {
         LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+        PlayerPrefs.SetString(localeKey, LocalizationSettings.SelectedLocale.Identifier.Code);
+    }
+
+    void LoadLocale()
+    {
+        // Load saved locale.
+        if (PlayerPrefs.HasKey(localeKey))
+        {
+            string localeCode = PlayerPrefs.GetString(localeKey);
+            Locale locale = LocalizationSettings.AvailableLocales.GetLocale(localeCode);
+            if (locale != null)
+            {
+                LocalizationSettings.SelectedLocale = locale;
+                return;
+            }
+        }
+        // Fallback to system locale.
+        Locale systemLocale = LocalizationSettings.AvailableLocales.GetLocale(Application.systemLanguage);
+        if (systemLocale != null)
+        {
+            LocalizationSettings.SelectedLocale = systemLocale;
+        }
     }
 }
