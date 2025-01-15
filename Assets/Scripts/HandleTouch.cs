753377c3403af9575018b13c7a3cf2113cec3613
shostakun <23522495+shostakun@users.noreply.github.com>
2025-01-15 17:01:03 +0900

    Resolved #27. Added train follow mode.

--- Assets/Scripts/HandleTouch.cs
+++ Assets/Scripts/HandleTouch.cs
@@ -18,20 +18,12 @@ public class HandleTouch : MonoBehaviour
     {
         if (!EventSystem.current.IsPointerOverGameObject())
         {
-            Vector3? worldPosition = GetWorldPosition(finger.ScreenPosition);
+            Vector3? worldPosition = CameraUtil.GetWorldPosition(finger.ScreenPosition);
             if (worldPosition.HasValue)
             {
                 TileHighlight highlight = Board.inst.GetTrack(worldPosition.Value)?.GetComponent<TileHighlight>();
                 if (highlight != null) highlight.PlaceObject();
             }
         }
-    }
-
-    public static Vector3? GetWorldPosition(Vector2 screenPosition)
-    {
-        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
-        Plane plane = new Plane(Vector3.up, Vector3.zero);
-        if (plane.Raycast(ray, out float distance)) return ray.GetPoint(distance);
-        return null;
     }
 }
